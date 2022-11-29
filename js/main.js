c0e1c6457dc5f4aaa3b06c63c6f436b16e2656fa
Camilo <camulocampillo@gmail.com>
2022-11-29 17:00:17 -0500

    actualizado

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let boton = document.querySelector(".main_svg") ;
+let svg = document.getElementById("svg");
+let respuseta = document.getElementById("carta-respuesta");
+
+boton.addEventListener("click" , respuesta );
+
+function respuesta() {
+    respuesta.style.display = "flex";
+}
