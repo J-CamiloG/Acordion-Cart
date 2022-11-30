fb2fedae9ae935ee2a265289402d62da6c1a06c8
Camilo <camulocampillo@gmail.com>
2022-11-30 16:15:41 -0500

    estructura v 1.1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let boton = document.querySelector(".main_svg") ;
 let svg = document.getElementById("svg");
-let respuseta = document.getElementById("carta-respuesta");
+let parrafo = document.getElementById("carta-respuesta");
 
 boton.addEventListener("click" , respuesta );
 
 function respuesta() {
-    respuesta.style.display = "flex";
+    parrafo.style.display = "flex";
+}
+function ocultar() {
+    parrafo.style.display = "none";
 }
